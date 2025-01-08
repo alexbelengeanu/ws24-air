92180ad3b0969f1cf4582a9e7b1e05400eca835f
Teodora Alexandrescu <t.alexandrescu@student.tugraz.at>
2025-01-08 10:35:34 +0100

    add actual image embeddings

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -2,30 +2,39 @@ import streamlit as st
 import requests
 import json
 import uuid
+import os
+import sys
+from src.embeddings.pretrained import FaceEmbeddings
+from src.storage.dataset import Dataset
+
+# print("Current Working Directory:", os.getcwd())
+
+# # Print Python path
+# print("Python Path:", sys.path)
 
 # Sidebar for navigation
 option = st.sidebar.selectbox("Choose Action", ["Insert Celebrity", "Search Celebrity"])
+
+em = FaceEmbeddings()
+
+dataset = Dataset(
+    img_folder_path="./data/img_align_celeba",
+    img_identity_map_path="./data/identity_CelebA.txt"
+)
+
+img_identity_collection = dataset.images_by_identity()
 
 # Insert Celebrity
 if option == "Insert Celebrity":
     st.header("Insert Celebrity")
     img_path = st.text_input("Image Path", value="./data/img_align_celeba/024291.jpg")
-    embedding = st.text_area("Embedding (comma-separated)", "-137.12767626, 83.62350658")
-    
     if st.button("Insert"):
         try:
-            embedding_list = [float(x.strip()) for x in embedding.split(',')]
-            # payload = {
-            #     "celeb_id": celeb_id,
-            #     "img_path": img_path,
-            #     "embedding": embedding_list
-            # }
-
             payload = json.dumps({
                      # unique celebrity ID
                       "celeb_id": str(uuid.uuid4()),
                       "img_path": img_path,
-                      "embedding": embedding_list
+                      "embedding": em.get_embedding(path=img_path)
                     })
             headers = {
               'Content-Type': 'application/json'
@@ -38,17 +47,19 @@ if option == "Insert Celebrity":
 # Search for similar celebrity
 if option == "Search Celebrity":
     st.header("Search Celebrity")
-    embedding = st.text_area("Embedding (comma-separated)", "-182.55377947, 56.77146782")
     max_results = st.number_input("Max Results", min_value=1, step=1, value=1)
     
     if st.button("Search"):
         try:
-            embedding_list = [float(x.strip()) for x in embedding.split(',')]
-            payload = {
-                "embedding": embedding_list,
-                "max_results": max_results
+            payload = json.dumps({
+                      "embedding": em.get_embedding(path=img_path),
+                      "max_results": max_results
+                    })
+            headers = {
+              'Content-Type': 'application/json'
             }
-            response = requests.post("http://localhost:5000/api/v1/search", json=payload)
+
+            response = requests.post("http://localhost:5000/api/v1/search", headers=headers, data=payload)
             st.json(response.json())
         except Exception as e:
             st.error(f"Error: {e}")
